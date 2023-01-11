aec9681d780d002fd43a7973d631b45645aa15a0
Lsepulvedap <92001537+Lsepulvedap@users.noreply.github.com>
2023-01-11 13:05:16 -0300

    add nodes
    Añadimos nodos y atributos. Hay que cambiar el hecho de que nodes_pos debe ser un indice, y que los indices deberían ir en orden

--- fourth_ attempt_ramification_pattern.py
+++ fourth_ attempt_ramification_pattern.py
@@ -21,7 +21,7 @@ s= 0.9
 
 #-----------------------------ARREGLOS PARA GUARDAR DATOS---------------------------------------------
 
-# edges=np.empty([0, 2])
+edges=np.empty([0, 2])
 
 T = np.array([[0,0]]) #define las posiciones ocupadas por cada particula
 
@@ -67,10 +67,10 @@ def edge_list( G, edges, T, source, target):
     nodes_pos= T[source] #?? <==> T[source, :] entregará un par ordenado con las coordenadas de source
     #así, iremos desde source hasta target
     
-    edges= np.append(edges, [source, target] , axis=0)
+    edges= np.append (edges, np.array([ [source, target] ]) , axis=0)
     
     G.add_node(target, pos= (nodes_pos[0], nodes_pos[1])) #añade un nodo con coordenadas x, y de source
-    
+    #revisar linea 72
     return G, edges 
 # end edge_list
 #%% 
@@ -79,11 +79,13 @@ def action(G, edges, nodes_pos, pos, Trace, ind_tmp, N):
     
     ''' Toma un grafo G y un arreglo edges que indica las conexiones que se van dando entre los nodos 
     indexados. Pos corresponde a la nueva posicion de la particula que decide moverse, por lo que corresponde
-    a un par odenado (x,y), y Trace el arreglo de todas las posiciones que se han ocupado.
+    a un par odenado (x,y), y Trace el arreglo de todas las posiciones que se han ocupado. Además
+    nodes_pos es el índice que indica la ubicacion del nodo en la traza.
     
     Por ultimo, ind_tmp y N son los indices de las particulas temporalmente activas y el numero de particulas, 
     respectivamente. ''' 
     
+
     pos += direction()
     if isintrace(pos, Trace):
         pass 
@@ -99,7 +101,7 @@ def action(G, edges, nodes_pos, pos, Trace, ind_tmp, N):
             
     G, edges= edge_list(G, edges, T, nodes_pos, N ) #source es un indice de la traza, y target tambien
     
-  
+  #nodes_pos es un indice 
             
          
     return G, Trace, N, edges #ind_part_activas 
@@ -114,13 +116,14 @@ while t < tmax  and len(ind_part_activas) >0  :
     
     t += h # tiempo= tiempo+ h
     ind_tmp = [] #guarda una posicion temporal que debe ser verificada en la traza
-    edges= [] 
+    
+
     for i in np.random.permutation(ind_part_activas) : 
         
          
         r= np.random.uniform(0,1) 
         
-        nodes_pos= T[i]
+        nodes_pos= T[i] #aqui esto no es un indice , y debería serlo
 
         if r< s:
         
@@ -132,17 +135,12 @@ while t < tmax  and len(ind_part_activas) >0  :
             G, T, N, edges =  action(G, edges, nodes_pos, pos, T, ind_tmp, N)
             
     ind_part_activas = ind_tmp
-   
     
-    # G.add_edges_from(edges)
-    
-    # pos_dictionary = nx.get_node_attributes(G, 'pos')
-    
-    print(ind_tmp)
 #%%
- 
-G.add_edges_from(edges)
-#pos= nx.get_node_attributes(G, 'pos')
+for i in range(len( T[:,0] ) ) : 
+    G.add_node(i, pos= (T[i,0], T[i,1]))
 
-#nx.draw_networkx(G, pos, with_labels=True, arrows=True )
+#%%
+x= nx.get_node_attributes(G, 'pos')
 
+nx.draw(G, x)
